b9f8cc34e70d28f5bfb9c5a5e51035db04196f28
Riasat Sk <riasat.sk@outlook.com>
2025-07-10 00:55:09 +0530

    Changes to be committed: 	modified:   src/lib/db/index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'], // Optional: useful during development
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
-
